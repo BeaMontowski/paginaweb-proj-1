88defb380b5a2665613153acee235ce4b948b74e
Beatriz <beatriz.montowski@escola.pr.gov.br>
2025-05-23 20:31:18 -0300

    sexto commit
    (tentando) alinhar cartões e começando em JavaScript

---
+++ FlashCards/perguntas.js
@@ -0,0 +1,14 @@
+criaCartao(
+    'Onde Van Gogh nasceu?',
+    'Ele nasceu em Zundert, na Holanda, em 30 de março de 1853.'
+)
+
+criaCartao(
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
